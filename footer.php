e8ec371b8c539c9f1332272743191e9da70b7451
Da-Woon Chung <dorapen@gmail.com>
2016-03-27 20:38:15 +0300

    Fix Aalto links.

--- footer.php
+++ footer.php
@@ -20,9 +20,9 @@
 				<?php endif; ?>
 
 				<div class="aalto-info">
-          <a href="#" class="aalto-link"><?php generate_aalto_logo( aalto_blogs_rgba( get_theme_mod( 'footer_textcolor' ) ?: 'FFF', 0.9 ) ); ?></a>
+          <a href="http://aalto.fi/en/" class="aalto-link" target="_blank"><?php generate_aalto_logo( aalto_blogs_rgba( get_theme_mod( 'footer_textcolor' ) ?: 'FFF', 0.9 ) ); ?></a>
 					<p class="copyright">&copy; <?php bloginfo( 'name' ); ?> <?php echo current_time( 'Y' ); ?> </p>
-					<p class="powered-by"><a href="http://blogs.aalto.fi" target="blank">Aalto Blogs</a> service provided by <a href="#" target="blank">Aalto IT</a></p>
+					<p class="powered-by"><a href="http://blogs.aalto.fi" target="_blank">Aalto Blogs</a> service provided by <a href="https://into.aalto.fi/display/enit/Homepage" target="_blank">Aalto IT</a></p>
 				</div>
 			</div>
     </div>
